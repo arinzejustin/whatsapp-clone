1201d0550277dab9eea72f0b4d541715ffb7088c
arinzejustinng <arinzejustinng@gmail.com>
2022-02-02 22:25:47 -0800

    Create conversation.dart

---
+++ lib/screens/conversation.dart
@@ -0,0 +1,16 @@
+List<String> imageUrls = [
+  'images/11.jpg',
+  "images/1.jpeg",
+  "images/7.jpg",
+  "images/5.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/11.jpg",
+  "images/10.jpg",
+  "images/9.jpg",
+  "images/8.png",
+  "images/12.jpg",
+  "images/do.jpg",
+  "images/images.jpg",
+  "images/download.jpg"
+];
